48b013366fbf7b0d19d50b6575885199139f317c
anchor-cable <forever.blue.moon@gmail.com>
2023-03-17 10:47:38 +0900

    feat: resp PONG

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream}, io::Write, fmt::Error};
+use std::io::Result;
 
-fn main() {
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     
@@ -8,12 +9,15 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all(b"+PONG\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
